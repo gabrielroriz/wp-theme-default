0112919d128cb4155e94c4d8d207ede559b89fcc
Gabriel Roriz <ogabrielroriz@gmail.com>
2021-09-11 09:24:13 -0300

    Atualizando versão e correções feitas.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+# 
+# BUILD THEME
+# 
+
 dir=`dirname $0`
 
 . ./config.sh
@@ -29,8 +33,31 @@ cp -r $dir/assets $dir/build
 
 rm -rf $dir/build/assets/images
 
-zipname=$(echo $THEME_NAME | tr '[:upper:]' '[:lower:]' | tr ' ' '_')_${THEME_VERSION};
+zipname=$(echo $THEME_NAME | tr '[:upper:]' '[:lower:]' | tr ' ' '-')-v${THEME_VERSION};
 
-cd build; zip -r ./$zipname.zip *
+cd build; 
+
+zip -q ./$zipname.zip * # Without LOG
+# zip -r ./$zipname.zip * #With LOG
 
 echo -e "\x1b[40mBuild done successfully.\x1b[0m"
+
+# 
+# UPDATE VERSION
+# 
+
+cd ..
+
+CONFIG_PATH="./config.sh"
+CONFIG_PATH__TEMP="./__config.sh"
+
+CURRENT_THEME_VERSION_VAR="THEME_VERSION=\"${THEME_VERSION}\"" # >> THEME_VERSION="1"
+NEW_THEME_VERSION_VAR="THEME_VERSION=\"$((${THEME_VERSION} + 1))\"" # ADD 1 TO VERSION >> THEME_VERSION="2"
+
+# Update Version in Another File "./__config.sh".
+sed 's/'$CURRENT_THEME_VERSION_VAR'/'$NEW_THEME_VERSION_VAR'/g' ${CONFIG_PATH} > ${CONFIG_PATH__TEMP}
+
+# Change new File to original File --> "./__config.sh" to "./config.sh".
+mv ${CONFIG_PATH__TEMP} ${CONFIG_PATH}
+
+echo -e "\x1b[40mVersion updated.\x1b[0m"
--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 THEME_NAME="Nome do Tema"
 THEME_AUTHOR="Gabriel Roriz"
 THEME_AUTHOR_URI="https://www.gabrielroriz.com.br"
-THEME_DESCRIPTION=""
-THEME_VERSION="0.1"
-
+THEME_DESCRIPTION="[Descrição...]"
+THEME_VERSION="3"
 
